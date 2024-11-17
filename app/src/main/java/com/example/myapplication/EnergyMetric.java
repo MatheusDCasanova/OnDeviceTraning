2ffcfd62ab18c01fcc9184133a0c44b24bfdb0a6
Matheus Casanova <matheuscasanova02@gmail.com>
2024-11-17 18:26:39 -0300

    Creates energy metric class for database

---
+++ app/src/main/java/com/example/myapplication/EnergyMetric.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+public class EnergyMetric {
+    public int epochs;
+    public int batches;
+    public int batchSize;
+    public String modelLink;
+    public String labelsLink;
+    public String featuresLink;
+    public long timeSpent;
+    public int modelSize;
+    public double energySpent;
+    public double sampleEnergy;
+    public double sampleTime;
+
+    public EnergyMetric(){}
+    public EnergyMetric(double energySpent, double sampleEnergy, double sampleTime, int epochs, int batches, int batchSize,
+                        String modelLink, String labelsLink, String featuresLink, long timeSpent, int modelSize) {
+        this.epochs = epochs;
+        this.batches = batches;
+        this.batchSize = batchSize;
+        this.modelLink = modelLink;
+        this.labelsLink = labelsLink;
+        this.featuresLink = featuresLink;
+        this.timeSpent = timeSpent;
+        this.modelSize = modelSize;
+        this.energySpent = energySpent;
+        this.sampleEnergy = sampleEnergy;
+        this.sampleTime = sampleTime;
+    }
+}
+
+
